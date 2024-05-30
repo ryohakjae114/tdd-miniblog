317f7a917b120919cbc35a6f536126f18532179e
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-05-30 14:51:48 +0900

    セットアップ

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -83,5 +83,5 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 
-  config.hosts << 'template_rails.test'
+  config.hosts << 'tdd-miniblog.test'
 end

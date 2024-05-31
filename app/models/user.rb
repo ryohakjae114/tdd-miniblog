316787f89cbb07376d59dca8d1dfe3d281588718
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-05-31 14:58:26 +0900

    userのnameカラムのバリデーションメッセージを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name, presence: true, length: { maximum: 20 }, format: { with: /^[0-9a-zA-Z]+$/, message: t('errors.user.name') }
+  validates :name, presence: true, length: { maximum: 20 }, format: { with: /^[0-9a-zA-Z]+$/, multiline: true, message: I18n.t('errors.user.name') }
 end
